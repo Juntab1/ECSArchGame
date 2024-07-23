54ed59a014714a641b49e53e0dec0c0a1f9ae7d1
Juntaro Bruttomesso <1092410@apps.nsd.org>
2024-07-22 21:06:38 -0700

    almost complete with entities and components

--- playerGame/Program.cs
+++ playerGame/Program.cs
@@ -30,6 +30,42 @@ namespace playerGame
             // };
 
 // create structs defining the constant components created above
+enum ComponentId
+{
+    rank,
+    name,
+    strength,
+    points
+}
+
+using Type = std::vector<ComponentId>;
+
+struct Archetype 
+{
+    Type type;
+    // this gives us a O(1). However, not helpful because extra space
+    // unordered_set<ComponenetId> type_set;
+    
+    // We always have less ComponentId than Archetype so having map is important
+    ArchetypeId id; 
+};
+
+using ArchetypeSet = unordered_set<ArchetypeId>;
+// gives us all the archtype ids that the component is in
+unordered_map<ComponentId, ArchetypeSet> component_index;
+
+// keeps track of the associated archtype with the entityId
+unordered_map<EntityId, Archetype&> entity_index;
+
+// find archetype for a list of components given
+unordered_map<Type, Archetype> archetype_index;
+
+bool has_component(EntityId entity, ComponenetId component)
+{
+    Archetype& archetype = entity_index[entity];
+    ArchetypeSet& archetype_set = component_index[component];
+    return archetype_set.count(archetype.id) != 0;
+}
 
 
 
